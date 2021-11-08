6d56f796487fdf429588e3504f40efef06a3a119
Rachel Sharp <rachel@hashicorp.com>
2021-11-08 16:55:22 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,23 @@
-resource "random_id" "random" {
-  keepers = {
-    uuid = uuid()
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
   }
-
-  byte_length = 11
- 
 }
 
-output "random" {
-  value = random_id.random.hex
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "nginx"
+  ports {
+    internal = 80
+    external = 8000
+  }
 }
